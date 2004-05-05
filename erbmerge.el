4e6ff83853b47dff495e1844d870e24491a1cc7c
deego <deego>
2004-05-05 19:54:15 +0000

    initial add, work in progress.

---
+++ erbmerge.el
@@ -0,0 +1,48 @@
+;;; erbmerge.el --- merge 2 bbdb's -- NOT YET IMPLEMENTED
+;; Time-stamp: <2004-05-05 15:51:22 deego>
+;; Copyright (C) 2004 D. Goel
+;; Emacs Lisp Archive entry
+;; Filename: erbmerge.el
+;; Package: erbmerge
+;; Author: D. Goel <deego@glue.umd.edu>
+;; Keywords:
+;; Version:
+;; URL: http://gnufans.net/~deego
+;; For latest version:
+
+
+ 
+;; This file is NOT (yet) part of GNU Emacs.
+ 
+;; This is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+ 
+;; This is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+ 
+
+;; See also:
+
+;;; Requires:
+(eval-when-compile (require 'cl))
+
+;;; Real Code:
+;; Use functions like fs-notes... See the code of erbforget-slowly..
+;; NOT YET IMPLEMENTED.
+
+
+(provide 'erbmerge)
+(run-hooks 'erbmerge-after-load-hook)
+
+
+
+;;; erbmerge.el ends here
