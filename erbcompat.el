320c9af0dc05a0eb2bdd5ba70ff5b8afb28636d1
hroptatyr <hroptatyr>
2004-06-20 20:01:11 +0000

    fs-describe-variable works again on XEmacsen

--- erbcompat.el
+++ erbcompat.el
@@ -1,5 +1,5 @@
 ;;; erbcompat.el --- Erbot GNU Emacs/XEmacs compatibility issues
-;; Time-stamp: <2004-03-28 12:19:00 deego>
+;; Time-stamp: <04/06/20 19:36:08 freundt>
 ;; Copyright (C) 2004 S. Freundt
 ;; Emacs Lisp Archive entry
 ;; Filename: erbcompat.el
@@ -30,7 +30,7 @@
 
 
 (defvar erbot-on-xemacs-p nil
-	"Whether erbot is run on xemacs.")
+  "Whether erbot is run on xemacs.")
 
 (setq erbot-on-xemacs-p
       (and (string-match "xemacs" emacs-version) t))
@@ -38,19 +38,17 @@
 
 ;;; local-variable-p stuff
 (or (and erbot-on-xemacs-p
-	 (defun erbcompat-local-variable-p (variable &optional buffer)
-	   "Just in compatibilty to GNU Emacs"
-	   (local-variable-p variable (or buffer (current-buffer)))))
-    (defalias 'erbcompat-local-variable-p 'local-variable-p))
+    (defun erbcompat-local-variable-p (variable &optional buffer)
+      "Just in compatibilty to GNU Emacs"
+      (local-variable-p variable (or buffer (current-buffer)))))
+  (defalias 'erbcompat-local-variable-p 'local-variable-p))
 
-
-;;; describe-variable stuff (currently b0rked on xemacs)
+;;; help-xref stuff
 (and erbot-on-xemacs-p
-   (defadvice erbutils-describe-variable (around erbot-on-xemacs activate)
-		 "Like describe-variable, but doesn't print the actual value."
-		 (format "This is %s run on XEmacs, atm describe-variable is n/a."
-						 erbot-nick)))
-     ;;; will later get on it
+     (defun help-setup-xref (&rest ignore))
+     (defun help-xref-button (&rest ignore))
+     (defun help-xref-stack (&rest ignore)))
+
 
 (provide 'erbcompat)
 
