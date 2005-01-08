12cf81d8b14e7e7a4455d02aee9f0091c9a04d9a
deego <deego>
2005-01-08 04:08:16 +0000

    itemize minor change

--- erbutils.el
+++ erbutils.el
@@ -1,5 +1,5 @@
 ;;; erbutils.el --- 
-;; Time-stamp: <2005-01-06 21:49:29 deego>
+;; Time-stamp: <2005-01-07 22:18:32 deego>
 ;; Copyright (C) 2002 D. Goel
 ;; Emacs Lisp Archive entry
 ;; Filename: erbutils.el
@@ -344,9 +344,10 @@ as many times as it returns a...
 
 
 (defvar erbutils-itemize-style
-  (list "[%s] %s,\n\n" "and also [%s] %s\n\n" "and [%s] %s,\n\n")
-  "Another choice, for example is 
-\(list \"[%s] %s\n\n\" \"[%s] %s\n\n\" \"[%s] %s,\n\n\")
+  (list "[%s] %s\n\n" "[%s] %s\n\n" "[%s] %s,\n\n")
+
+  "Another good choice, for example, and used by petekaz's petebot, is  
+  \(list \"[%s] %s,\n\n\" \"and also [%s] %s\n\n\" \"and [%s] %s,\n\n\")
 ")
 
 (defun erbutils-itemize (result &optional N shortenedp style)
